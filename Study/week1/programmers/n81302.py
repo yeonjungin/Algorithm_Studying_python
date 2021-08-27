7c2201c382e6bdb499b43ad465044bf8c17eb0d6
yeonjungin <0206dus@gmail.com>
2021-08-27 17:00:59 +0900

    21.8.27 Solving Update

--- Study/week1/programmers/n81302.py
+++ Study/week1/programmers/n81302.py
@@ -1,40 +1,50 @@
-'''
-거리두기 확인하기 : Programmers Level 2.
-'''
 from collections import deque
 
+D=((-1,0),(1,0),(0,-1),(0,1)) #상,하,좌,우 / 성능개선을 위해 튜플로 정의
 
-def bfs(array):
-    pass
+def bfs(place,row,col):
+    visited=[[False for _ in range(5)] for _ in range(5)]
+    queue=deque()
+    visited[row][col]=True
+    queue.append((row,col,0)) #행,열,좌표
+    while queue:
+        now=queue.popleft()
+        if now[2]>2:
+            continue
+        if now[2]!=0 and place[now[0]][now[1]]=='P': # 자기 자신(P)가 아닌 다른 P를 만났다.
+            return False
+        for i in range(4):
+            nx=now[0]+D[i][0]
+            ny=now[1]+D[i][1]
+            if nx<0 or nx>4 or ny<0 or ny>4:
+                continue
+            if visited[nx][ny]:
+                continue
+            if place[nx][ny]=='X':
+                continue
+            visited[nx][ny]=True
+            queue.append((nx,ny,now[2]+1))
+            
+    return True
+    
+
+def check(place):
+    for i in range(5):
+        for j in range(5):
+            if place[i][j]=='P':
+                if bfs(place,i,j)==False:
+                    # 거리두기 지키지 x
+                    return False
+                
+    return True
+        
 
 def solution(places):
-    dx = [-1, -1, 0, 1, 1, 1, 0, -1]
-    dy = [0, 1, 1, 1, 0, -1, -1, -1]
-    button = True
-    answer = []
-    queue=deque()
-    for array in places:
-        button=True
-        for i in range(5):
-            for j in range(5):
-                if array[i][j] == 'P':
-                    for k in range(8):
-                        ni = i + dx[k]
-                        nj = j + dy[k]
-                        if ni >= 0 and ni < 5 and nj >= 0 and nj < 5:
-                            if array[ni][nj] == 'P':
-
-                if button is False:
-                    break
-            if button is False:
-                break
-
-        if button:
+    answer=[]
+    for place in places:
+        if check(place):
             answer.append(1)
         else:
             answer.append(0)
-
+            
     return answer
-
-array=[["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
-print(solution(array))
